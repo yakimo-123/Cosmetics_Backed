713d9d228f0033deb3cbea0c399eb7454319ef51
Hung <lequanghung471@gmail.com>
2025-06-10 09:50:58 +0700

    fun add email service

---
+++ src/main/java/org/cosmetic/com/service/EmailService.java
@@ -0,0 +1,8 @@
+package org.cosmetic.com.service;
+
+import java.io.File;
+
+public interface EmailService {
+    void sendSimpleEmail(String to, String subject, String text);
+    void sendEmailWithAttachment(String to, String subject, String text, File attachment);
+}
---
+++ src/main/java/org/cosmetic/com/service/impl/EmailServiceImpl.java
@@ -0,0 +1,47 @@
+package org.cosmetic.com.service.impl;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.cosmetic.com.service.EmailService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+@AllArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+
+
+    @Override
+    public void sendSimpleEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+
+
+    @Override
+    public void sendEmailWithAttachment(String to, String subject, String text, File file) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+            helper.addAttachment(file.getName(), file);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Can't send mail with attachment", e);
+        }
+    }
+}
